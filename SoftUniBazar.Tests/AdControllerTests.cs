cbaf29fdf0414585cf4305c14d1cc3f35564fdff
Georgi Mutafov <georgimutafovbg@gmail.com>
2025-06-26 11:15:21 +0300

    Update AdControllerTests.cs

--- SoftUniBazar.Tests/AdControllerTests.cs
+++ SoftUniBazar.Tests/AdControllerTests.cs
@@ -10,7 +10,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using SoftUniBazar.Data.Models;
-//test1
+//test123
 namespace SoftUniBazar.Tests
 {
     [TestFixture]
